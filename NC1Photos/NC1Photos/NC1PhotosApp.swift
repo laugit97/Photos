dd1924f77c8dc4ec45493e262680e61b2e2eaf9a
Laura Bognanni <bognanni.laura@gmail.com>
2022-11-16 16:58:43 +0100

    Created the Xcode Project based on an empty App Template

---
+++ NC1Photos/NC1Photos/NC1PhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1PhotosApp.swift
+//  NC1Photos
+//
+//  Created by Laura Bognanni on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1PhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
